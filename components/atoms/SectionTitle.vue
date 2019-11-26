5fa5005bd778b8d23e2ab8b18be2d4209c724c9c
tussi5969 <sakura.0304.0226@gmail.com>
2019-11-26 18:09:52 +0900

    set section title

--- components/atoms/SectionTitle.vue
+++ components/atoms/SectionTitle.vue
@@ -1,13 +1,20 @@
 <template lang="pug">
   .SectionTitle
     .title_wrapper(v-if="title === 'ThreeFeatures'")
-      h2 3つの特徴
+      .threefeatures
+        Three.threefeatures_three
+        h2.threefeatures_other つの特徴
     .title_wrapper(v-else)
       h2 {{title}}
 </template>
 <script>
 // ScheduleとかInfomationなど、セクションのタイトルを作る。「3つの特徴」だけ変則的
+import Three from '~/assets/images/three.svg?inline'
+
 export default {
+  components: {
+    Three
+  },
   props: {
     title: {
       type: String,
@@ -16,4 +23,6 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~/assets/scss/organisms/SectionTitle.scss';
+</style>
--- components/organisms/SecThreeFeatures.vue
+++ components/organisms/SecThreeFeatures.vue
@@ -3,11 +3,15 @@
     SectionTitle(:title="'ThreeFeatures'")
 </template>
 <script>
+// components
 import SectionTitle from '~/components/atoms/SectionTitle.vue'
+// svg
 export default {
   components: {
     SectionTitle
   }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~/assets/scss/organisms/SecThreeFeatures.scss';
+</style>
