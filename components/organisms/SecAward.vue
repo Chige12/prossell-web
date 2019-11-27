7172aa85b47d07d3fc9429ecc3353c9ffdbd7849
chige12 <tige1215.technic@gmail.com>
2019-11-27 21:39:13 +0900

    SecAward pug

--- components/organisms/SecAward.vue
+++ components/organisms/SecAward.vue
@@ -1,13 +1,40 @@
 <template lang="pug">
   .SecAward
     SectionTitle(:title="'Award'")
+    .container
+      .main_wrapper
+        .award_txt
+          .award_txt__row
+            .award_txt__row__property 表彰式
+            .award_txt__row__contents
+              p 2020/03/14、新潟県長岡市 <a href="https://www.nadec-base.jp/">NaDeC BASE</a> にて開催
+              | 優秀者には交通費・宿泊費等、上限2万円の支給
+          .award_txt__row
+            .award_txt__row__property 賞金
+            .award_txt__row__contents
+              table
+                tr
+                  td 最優秀賞：
+                  td 賞金10万円
+                tr
+                  td 企業賞：
+                  td 当日までのお楽しみ
+        .award_img
+          AwardSvg.award_img__svg
+          .award_img__cap designed by Payungkead from Flaticon
 </template>
 <script>
 import SectionTitle from '~/components/atoms/SectionTitle.vue'
+import AwardSvg from '~/assets/images/award.svg?inline'
 export default {
   components: {
-    SectionTitle
+    SectionTitle,
+    AwardSvg
   }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main_wrapper {
+  @include flex();
+}
+</style>
