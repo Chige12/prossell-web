c055f5e96f5c65c52979c60048958e16dc1743b1
chige12 <tige1215.technic@gmail.com>
2019-11-30 17:24:40 +0900

    add middle background triangle

--- components/atoms/BackgroundTriangleMiddle.vue
+++ components/atoms/BackgroundTriangleMiddle.vue
@@ -1,3 +1,28 @@
 <template lang="pug">
-  
+  .BackgroundTriangleMiddle
+    TriSvgWhite.back_white
 </template>
+<script>
+import TriSvgWhite from '~/assets/images/tri_3.svg?inline'
+
+export default {
+  components: {
+    TriSvgWhite
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.BackgroundTriangleMiddle {
+  @include absolute($bottom: 0, $left: 0);
+  width: 100%;
+  height: 100%;
+}
+.back_white {
+  @include absolute($bottom: 0, $left: 0);
+  display: block;
+  padding-top: 200px;
+  z-index: -1;
+  height: 100%;
+}
+</style>
