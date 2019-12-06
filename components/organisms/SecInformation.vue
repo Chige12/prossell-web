c1407e1f2e44434d1218c35fee612893338100a1
tussi5969 <sakura.0304.0226@gmail.com>
2019-12-06 23:33:38 +0900

    delete v-html

--- components/organisms/SecInformation.vue
+++ components/organisms/SecInformation.vue
@@ -2,41 +2,33 @@
   .SecInfomation
     SectionTitle(:title="'Information'")
     .container
-      .information_lists(v-if="$mq !== 'sm'")
-        .information_list(v-for="(info, info_key) in informations" :key="`info_${info_key}`" :style="`padding-left: ${info_key * 34.4}px`")
-          .information_list_theme {{info.theme}}
-          .information_list_content(v-html="info.content")
-      .information_lists(v-else)
-        .information_list(v-for="(info, info_key) in informations" :key="`info_${info_key}`")
-          .information_list_theme {{info.theme}}
-          .information_list_content(v-html="info.content")
+      .information_lists
+        .information_list.participate
+          p.information_list_theme 参加対象者
+          p.information_list_content 大学生、 大学院生、 高専生、 専攻科生
+        .information_list.view
+          p.information_list_theme 審査の視点
+          .information_list_content
+            p.information_list_content--text 実現性、 社会貢献度、 市場の成長性、
+            | 独創性、 スケジュール達成度<sup>※</sup>
+            p.information_list_content--caution ※スケジュールに沿ったタスクの達成率も評価に加味されます。
+        .information_list.judge
+          p.information_list_theme 審査員
+          p.information_list_content 未定
+        .information_list.management
+          p.information_list_theme 運営
+          .information_list_content
+            p.information_list_content--sponsor 主催：
+              a(href = "") Prossell
+            p.information_list_content--coorporate 協力：
+              a(href = "https://n-wakamonokikou.net/") ながおか・若者・しごと機構
+
 </template>
 <script>
 import SectionTitle from '~/components/atoms/SectionTitle.vue'
 export default {
   components: {
     SectionTitle
-  },
-  data() {
-    return {
-      informations: [
-        {
-          theme: '参加対象者',
-          content: '<p>大学生、 大学院生、 高専生、 専攻科生</p>'
-        },
-        {
-          theme: '審査の視点',
-          content:
-            '<p>実現性、 社会貢献度、 市場の成長性、 独創性、 スケジュール達成度<sup>※</p><p class="caution" style="font-size: 1.0rem; position: absolute; left: 6px;">※スケジュールに沿ったタスクの達成率も評価に加味されます。</p>'
-        },
-        { theme: '審査員', content: '<p>未定</p>' },
-        {
-          theme: '運営',
-          content:
-            '<p>主催：<a>Prossell</a>　協力：<a href="https://n-wakamonokikou.net/">ながおか・若者・しごと機構</a></p>'
-        }
-      ]
-    }
   }
 }
 </script>
