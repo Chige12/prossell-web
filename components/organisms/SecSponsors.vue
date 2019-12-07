6a40ba557801124b0e3d9ccfa9bc29b9dd57d155
chige12 <tige1215.technic@gmail.com>
2019-12-07 15:00:43 +0900

    sponsors data hidden && display corporates link

--- components/organisms/SecSponsors.vue
+++ components/organisms/SecSponsors.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-  .SecSponsors(v-if="!publish")
+  .SecSponsors
     SectionTitle(:title="'Sponsors'")
-    .container
+    .container(v-if="publish")
       .sponsors_list
         .sponsor_box(v-for="(spA, spA_key) in sponsors_A" :key="`spA_key${spA_key}`").pran_A
           a(:href="spA.link")
@@ -17,12 +17,26 @@
           a(:href="spC.link")
             .sponsor_logo
             .sponsor_name <span>Pran C</span> {{spC.name}}
+
+    .container(v-else)
+      nuxt-link(to="/corporates")
+        CommingSoonSvg.comming_soon
+      p.prease_weit 情報公開までもうしばらくお待ちください。
+      .sponsor_link
+        h3 【企業の皆様へ】
+        p スポンサーは随時募集しています。
+        p スポンサー用の情報については 
+          nuxt-link(to="/corporates") 企業の方へ 
+          | をご覧ください。
 </template>
 <script>
 import SectionTitle from '~/components/atoms/SectionTitle.vue'
+import CommingSoonSvg from '~/assets/images/comming_soon.svg?inline'
+
 export default {
   components: {
-    SectionTitle
+    SectionTitle,
+    CommingSoonSvg
   },
   data() {
     return {
@@ -134,6 +148,36 @@ a:visited {
       display: block;
       padding-right: 0;
     }
+  }
+}
+
+// pre release
+
+.container {
+  text-align: center;
+}
+.comming_soon {
+  width: 260px;
+  transition: 0.3s $ease-out-1;
+  .comming_soon_cls-2 {
+    fill: $gray;
+  }
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+.prease_weit {
+  margin-top: 8px;
+  color: $gray-txt;
+}
+.sponsor_link {
+  margin-top: 52px;
+  h3 {
+    margin: 12px 0;
+  }
+  a {
+    text-decoration: underline;
+    text-underline-position: under;
   }
 }
 </style>
