eada51daa50de6d86fcb42146e87ad74f1e97dd6
tussi5969 <sakura.0304.0226@gmail.com>
2019-12-10 23:29:16 +0900

    update name plan

--- pages/corporates.vue
+++ pages/corporates.vue
@@ -4,7 +4,7 @@
     SubPageTopTitle(:title="'For corporates'")
     .container
       .for_corp_message オンコン（略称）では、スポンサーを随時募集中です。学生のスキルアップや成長を応援しませんか？
-    SectionTitle(:title="'Prans'")
+    SectionTitle(:title="'Plans'")
     .container
       .main_wrapper
         .pran_discription オンコンでは、3つのスポンサープランを提案させていただいております。
@@ -12,9 +12,9 @@
           table
             thead
               th.contents
-              th.center.center_h Pran A
-              th.center.center_h Pran B
-              th.center.center_h Pran C
+              th.center.center_h Plan A
+              th.center.center_h Plan B
+              th.center.center_h Plan C
             tbody
               tr.pran_table_tr(v-for="trk in table_data.length" :key="`corp_trk${trk}`")
                 td.contents {{table_data[trk-1][0]}}
